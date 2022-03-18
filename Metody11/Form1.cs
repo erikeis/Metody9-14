438a2975b0e2fef48f0288889e5a742229a56ba8
erikeis <eise.05@A321-15>
2022-03-18 10:24:40 +0100

    Metody11

--- Metody11/Form1.cs
+++ Metody11/Form1.cs
@@ -16,5 +16,33 @@ namespace Metody11
         {
             InitializeComponent();
         }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int PocetSlov(string s1, out string s2)
+        {
+            char[] oddelovace = { ' ' };
+            string[] pole = s1.Split(oddelovace, StringSplitOptions.RemoveEmptyEntries);
+
+            int index = 0;
+            while (index < s1.Length)
+            {
+                if (Char.IsDigit(s1[index]))
+                {
+                    s1.Remove(index, 1);
+                }
+            }
+            s2 = s1;
+            return pole.Length;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            string novyRetezec;
+            PocetSlov(str, out novyRetezec);
+            MessageBox.Show("Pocet slov je " + PocetSlov(str, out novyRetezec).ToString() + " a novy retezec je " + novyRetezec);
+        }
     }
 }
