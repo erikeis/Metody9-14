ea03a710d57c6007b1d992cbb599aac722e40bf9
erikeis <eise.05@A321-15>
2022-03-18 10:55:31 +0100

    Metody13

--- Metody13/Form1.cs
+++ Metody13/Form1.cs
@@ -16,5 +16,48 @@ namespace Metody13
         {
             InitializeComponent();
         }
+        private bool JeAlfanum(string vstup, out int pocetMalych, out int pocetVelkych, out int pocetJinych)
+        {
+            bool vstupJeAlfanum = false;
+            pocetMalych = 0;
+            pocetVelkych = 0;
+            pocetJinych = 0;
+            foreach (char znak in vstup)
+            {
+                if (char.IsLetterOrDigit(znak))
+                {
+                    vstupJeAlfanum = true;
+                    if (char.IsUpper(znak))
+                    {
+                        ++pocetVelkych;
+                    }
+                    else if (char.IsLower(znak))
+                    {
+                        ++pocetMalych;
+                    }
+                }
+                else
+                {
+                    ++pocetJinych;
+                    vstupJeAlfanum = false;
+                }
+            }
+            return vstupJeAlfanum;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string retezec = textBox1.Text;
+            int pocetM;
+            int pocetV;
+            int pocetJ;
+            if (JeAlfanum(retezec, out pocetM, out pocetV, out pocetJ))
+            {
+                MessageBox.Show("Retezec je alfanumerum.");
+            }
+            else MessageBox.Show("Retezec neni alfanumericky.\n" + "Pocet velkych pismen: " + pocetV + "\nPocet malych pismen: " + pocetM + "\nPocet jinych znaku: " + pocetJ);
+        }
     }
 }
